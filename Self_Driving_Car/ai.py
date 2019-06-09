72596596b498b34aa43679043f336d58f63e5694
Shivaansh Prasann <shivaanshseal@gmail.com>
2019-06-09 17:41:53 -0400

    Deep Q Learning Class created

--- Self_Driving_Car/ai.py
+++ Self_Driving_Car/ai.py
@@ -32,9 +32,10 @@ class DriveMind(nn.Module): #inherits from nn.Module
         #creating full connections between layers
         #full connection: all neurons of one layer connected to all neurons of another layer
         #params: no. on neurons in each layer (L and R)
-        #30 derived by experimentation, is number of neurons in Hidden Layer. Experiment with this, see AI behavior
+        
         self.input_hidden_conn = nn.Linear(input_size, 30)
         self.hidden_output_conn = nn.Linear(30, poss_actions)
+        #30 derived by experimentation, is number of neurons in Hidden Layer. Experiment with this, see AI behavior
 
     """
     method: performs forward propagation
@@ -47,3 +48,71 @@ class DriveMind(nn.Module): #inherits from nn.Module
         x = F.relu(self.input_hidden_conn(input_state))
         q_values = F.relu(self.hidden_output_conn(x))
         return q_values
+
+## Class: Implementing Experience Replay
+class RevisitMemory(object):
+
+    """
+    method: constructor
+    param self: the referenced object
+    param capacity: number of experiences/transitions
+    """
+    def __init__(self, capacity):
+
+        self.capacity = capacity
+        self.memory = [] #contains all previous transition, populated when a future state is visited
+
+    """
+    method: adds state (experience) to memory list, ensures memory has 100 transitions
+    param self: the referenced object
+    param event: experience to be stored in memory
+    """
+    def push(self, event):
+        #transition format: (last state st, new state st+1, last action at, last reward rt)
+        self.memory.append(event)
+        if(len(self.memory) > self.capacity):
+            del self.memory[0]
+
+    """
+    method: return random samples from memory
+    param self: the referenced object
+    param sample_size: size of sample to be returned
+    """
+    def sample(self, sample_size):
+        #use random library method to randomly extract a sample of certain size from memory
+        samples = zip(*random.sample(self.memory, sample_size))
+
+        #FORMAT: STATE, ACTION, REWARD
+        #take samples, concatenate wrt first dimension, map to PyTorch variables
+        return map(lambda x: Variable(torch.cat(x, 0)), samples)
+
+
+## Class: Implement Deep Q learning model
+class Dqn():
+
+    """
+    method: constructor
+    param self: reference to the object
+    param input_size: number of input neurons in the NN, which together describe input_state of environment, arg to init function
+    param poss_actions: number of neurons in output layer
+    param gamma: learning rate for the q-learning model
+    """
+    def __init__(self, input_size, poss_actions):
+        self.gamma = gamma
+        self.reward_window = [] #average of 100 rewards, should increase over time
+        self.model = DriveMind(input_size, poss_actions) #instance of NN
+        self.memory = RevisitMemory(100000) #100000 transitions instance of memory
+        self.optimizer = optim.Adam(self.model.parameters(), lr = 0.001) #connect Adam optimizer to NN
+        
+        #STATE FORMAT: lSensor, RSensor, FSensor, Orientation, -Orientation
+        self.last_state = torch.Tensor(input_size).unsqueeze(0) #unsqueeze creates a fake dimension for tensor
+
+        self.last_action = 0
+        self.last_reward = 0.0
+
+    """
+    method: decides which action to play
+    param self: the referenced object
+    param : 
+    """
+    def select_action(self, ):
